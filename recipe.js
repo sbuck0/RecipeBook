65a1aa097b86d8eeb502c766f1cdf08bb370fced
Samson Buck <sbuck0@umd.edu>
2022-05-14 11:26:45 -0400

    api changes and some other fixes

--- recipe.js
+++ recipe.js
@@ -1,15 +1,25 @@
 process.stdin.setEncoding("utf8");
 
-let http = require("http");
-let express = require("express"); /* Accessing express module */
-let app = express(); /* app is a request handler function */
-let fs = require("fs");
-let bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+import fetch from "node-fetch"
+import http from "http"
+import express from "express" 
+import bodyParser from "body-parser"
+import { MongoClient, ServerApiVersion } from "mongodb"
+import path from "path"
+import dotenv from "dotenv"
+import {fileURLToPath} from 'url';
+import AsyncLock from "async-lock"
+
+var lock = new AsyncLock()
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(dirname, ".env") });
+let app = express();
 var globalUsername = "";
 var globalItems;
+var recipeIds = []
+var recipesWithInformation = []
+var titles = []
 
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
@@ -44,9 +54,10 @@ process.stdin.on("readable", function () {
   }
 });
 
-app.set("views", path.resolve(__dirname, "templates"));
+app.set("views", path.resolve(dirname, "Templates"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use( express.static( "public" ) );
 
 app.get("/", (request, response) => {
   response.render("index");
@@ -70,19 +81,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/processSearchRecipe", async (request, response) => {
   let { username, queryTerm } = request.body;
   globalUsername = username;
+  titles = []
   //   This is an array that is returned by API when we search for a recipe
-  recipeQueries = searchRecipes(queryTerm);
-  globalItems = recipeQueries;
-  recipeCheckboxes = generateRecipeChecklist(recipeQueries);
+  lock.acquire("", async function() {
+    await searchRecipes(queryTerm);
+  }, []).then(function() {
+    let recipeQueries = recipesWithInformation;
+    globalItems = recipeQueries;
+    var recipeCheckboxes = generateRecipeChecklist(recipeQueries);
 
-  let variables = {
-    username: username,
-    email: email,
-    recipeResults: recipeCheckboxes,
-    homeWebpage: `<a href="http://localhost:${portNumber}">HOME</a>`,
-  };
+    let variables = {
+      username: username,
+      searchTerm: queryTerm,
+      recipeResults: recipeCheckboxes,
+      homeWebpage: `<a href="http://localhost:${portNumber}">HOME</a>`,
+    };
 
-  response.render("results", variables);
+    response.render("results", variables);
+  });
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -97,10 +113,10 @@ app.post("/viewRecipeBook", async (request, response) => {
       serverApi: ServerApiVersion.v1,
     });
     let foundUser;
-    let getRecipeObjects = recipe1.reduce(
-      (previousValue, currentValue) => previousValue.push(globalItems[Number(currentValue)]),
-      []
-    );
+    let getRecipeObjects = []
+    for (let i = 0; i < recipe1.length; i++) {
+        getRecipeObjects.push(globalItems[Number(recipe1[i])])
+    }
     try {
       await client.connect();
       await insertOrUpdateUser(client, databaseAndCollection, globalUsername, getRecipeObjects);
@@ -114,7 +130,7 @@ app.post("/viewRecipeBook", async (request, response) => {
       recipeList: generateRecipeBook(foundUser),
       homeWebpage: `<a href="http://localhost:${portNumber}">HOME</a>`,
     };
-    response.render("applicantsData", variables);
+    response.render("recipebook", variables);
   } else {
     globalUsername = username;
     console.log("NO RECIPE1 FOUND");
@@ -133,11 +149,19 @@ app.post("/viewRecipeBook", async (request, response) => {
     } finally {
       await client.close();
     }
-    let variables = {
+    if (foundUser === null) {
+      let variables = {
+        error: `There is no user with the username ${username} that has recipes saved.`,
+        homeWebpage: `<a href="http://localhost:${portNumber}">HOME</a>`,
+      };
+      response.render("error", variables)
+    } else {
+      let variables = {
       recipeList: generateRecipeBook(foundUser),
       homeWebpage: `<a href="http://localhost:${portNumber}">HOME</a>`,
-    };
-    response.render("applicantsData", variables);
+      };
+      response.render("recipebook", variables);
+    }
   }
 });
 
@@ -153,7 +177,7 @@ async function insertOrUpdateUser(
     .collection(databaseAndCollection.collection)
     .findOne(filter);
   if (cursor) {
-    getUserRecipes = cursor.recipeList;
+    let getUserRecipes = cursor.recipeList;
     getUserRecipes = getUserRecipes.concat(addRecipes);
     const result = await client
       .db(databaseAndCollection.db)
@@ -188,25 +212,82 @@ async function lookUpOneEntry(client, databaseAndCollection, inputUser) {
   }
 }
 
-async function lookUpMany(client, databaseAndCollection, gpa) {
-  let filter = { gpa: { $gte: gpa } };
-  const cursor = client
-    .db(databaseAndCollection.db)
-    .collection(databaseAndCollection.collection)
-    .find(filter);
-
-  const result = await cursor.toArray();
-  return result;
+async function searchRecipes(recipeTerm) {
+  recipeIds = []
+  recipesWithInformation = []
+  let query = String(recipeTerm)
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'eee60225dfmsh483cdc041112e38p1614f2jsn1e3e3516465d'
+    }
+  };
+  await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${query}`, options)
+    .then(response => response.json())
+    .then(response => processids(response))
+    .then(async function(response) {
+      let end = 5
+      let start = 0
+      while (end < recipeIds.length && recipesWithInformation.length < 5) {
+        for (let i = start; i < recipeIds.length && i < end; i++) {
+          await getRecipeObjectById(recipeIds[i])
+        }
+        start = end
+        end += 1
+      }
+    })
+    .catch(err => console.error(err));
+  
 }
 
-/* SAMSON YOUR PART IS DOWN HERE */
-function searchRecipes(recipeTerm) {
-  /* Return array of five or so recipe Objects */
+function processids(response) {
+  let ids = []
+  if (response !== undefined) {
+      for(let i = 0; i < response["results"].length && i < 15; i++){
+          let data = {}
+          data["id"] = response["results"][i]["id"]
+          ids.push(data)
+      }
+  }
+  recipeIds = ids
 }
 
-function getRecipeObjectById(id) {
-  /* Return the entire recipeObject in the API given its id in the first query */
+async function getRecipeObjectById(id) {
+  const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+          'X-RapidAPI-Key': 'eee60225dfmsh483cdc041112e38p1614f2jsn1e3e3516465d'
+      }
+  };
+  if (id !== undefined && id !== null) {
+      await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id["id"]}/information`, options)
+      .then(response => response.json())
+      .then(response => processRecipes(response))
+      .catch(err => console.error(err));
+  }
 }
+
+function processRecipes(response) {
+  let data = {}
+  if (response !== undefined) {
+      data["title"] = response["title"]
+      data["url"] = response["sourceUrl"]
+      data["recipe"] = response["summary"]
+  }
+  let contains = false
+  for(let i = 0; i < titles.length; i++) {
+    if (titles[i] == data["title"]) {
+      contains = true
+    }
+  }
+  if (!contains) {
+    recipesWithInformation.push(data)
+    titles.push(data["title"])
+  }
+}
+
 
 function generateRecipeChecklist(recipeList) {
   let checklistHTML = "";
@@ -221,7 +302,7 @@ function generateRecipeBook(foundUser) {
   let listHTML = "";
   foundUser.recipeList.forEach(
     (elem) => 
-    (checklistHTML += `<li><a href="${elem.sourceUrl}">${elem.title}</a></li>`)
+    (listHTML += `<li><a href="${elem.url}">${elem.title}</a></li>`)
     );
     return listHTML;
 }
